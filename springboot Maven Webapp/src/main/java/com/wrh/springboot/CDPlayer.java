d0c76c4be986f3297a783cba67d6ff118166f247
yitianliangjiao <wrh@163.com>
2018-09-16 17:33:24 +0800

    rebase 000

--- springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
+++ springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
@@ -12,7 +12,7 @@ public CDPlayer(Sgtpeppers sp) {
 }
 public CDPlayer(){
 	System.out.println("111");
-	System.out.println("222");
+System.out.println("222");
 	
 	System.out.println("555");
 	System.out.println("222");
