03c8456fbf34160344c955422fda3613ff8fc912
yitianliangjiao <wrh@163.com>
2019-05-10 15:10:46 +0800

    ceshi

--- springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
+++ springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
@@ -17,13 +17,6 @@ public CDPlayer(Sgtpeppers sp) {
 }
 public CDPlayer(){
 	System.out.println("111");
-	
-	System.out.println("555");
-	System.out.println("222");
-	
-	System.out.println("333");
-	System.out.println("444");
-	System.out.println("222");
 }
 @Override
 public void play() {
