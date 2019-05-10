3a2c1441781e099ee26b1bc3547362fa6a8b74cd
yitianliangjiao <wrh@163.com>
2019-05-10 12:28:58 +0800

    Revert "测试git"
    This reverts commit d8efed56ca47e2bfbc3a8731ed90ca1e419d1ec9.

--- springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
+++ springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
@@ -17,6 +17,13 @@ public CDPlayer(Sgtpeppers sp) {
 }
 public CDPlayer(){
 	System.out.println("111");
+	
+	System.out.println("555");
+	System.out.println("222");
+	
+	System.out.println("333");
+	System.out.println("444");
+	System.out.println("222");
 }
 @Override
 public void play() {
