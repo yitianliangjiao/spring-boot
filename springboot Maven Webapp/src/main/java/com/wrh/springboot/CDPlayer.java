3d1e0147dd0fcd423d3d70fb7e3839948acad07a
yitianliangjiao <wrh@163.com>
2019-05-10 15:46:22 +0800

    测试

--- springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
+++ springboot Maven Webapp/src/main/java/com/wrh/springboot/CDPlayer.java
@@ -16,7 +16,7 @@ public CDPlayer(Sgtpeppers sp) {
 	System.out.println(sp.toString());
 }
 public CDPlayer(){
-	System.out.println("111");
+	System.out.println("111222222222");
 	System.out.println("222");
 	System.out.println("222");
 	System.out.println("222");
